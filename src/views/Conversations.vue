8d2e1f2e1b6ffde7c16fb57c7c198f02cdca85fe
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2020-02-28 17:18:52 -0500

    Create `/conversations` view skeleton (#2)
    * Create `Conversations` view
    * Create `ConversationsHome` component
    * Create routes for `/conversations` and `/conversations/:id`

---
+++ src/views/Conversations.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="conversations">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Conversations',
+};
+</script>
+
+<style>
+.conversations {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+</style>
