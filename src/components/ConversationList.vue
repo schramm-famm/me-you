c84c43f12525889bbf48780af2017cca80d9b55b
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2020-03-03 21:46:59 -0500

    Create `ConversationList` component

---
+++ src/components/ConversationList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="side">
+    <div class="header">
+      <h1>{{ username }}</h1>
+    </div>
+    <div class="conversation-list">
+      <ConversationItem
+        v-for="conversation in conversations"
+        :key="conversation.id"
+        :id="conversation.id"
+      ></ConversationItem>
+    </div>
+  </div>
+</template>
+
+<script>
+import ConversationItem from './ConversationItem.vue';
+
+function username() {
+  return this.$store.state.user.name;
+}
+
+function conversations() {
+  return this.$store.state.conversations;
+}
+
+export default {
+  name: 'ConversationList',
+  components: {
+    ConversationItem,
+  },
+  computed: {
+    username,
+    conversations,
+  },
+};
+</script>
+
+<style>
+.side {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 25%;
+  min-width: 7.5em;
+  overflow: hidden;
+  border-right: solid lightgrey thin;
+}
+
+.conversation-list {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 100%;
+  overflow: scroll;
+}
+</style>
