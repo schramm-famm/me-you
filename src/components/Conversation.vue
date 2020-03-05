b8a47f3f73440646ba4a013e8abd8c4f6cb61994
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2020-03-04 20:39:58 -0500

    Implement real-time collaboration
    * Add `diff-match-patch` dependency
    * Setup WebSocket connection to `patches` service
    * Implement sending and applying patches
    * Implement patch versioning logic (#6)

--- src/components/Conversation.vue
+++ src/components/Conversation.vue
@@ -9,15 +9,47 @@
         </div>
       </div>
     </div>
-    <div id="conversation-body" class="body" contenteditable="true">
+    <div
+      id="conversation-body"
+      class="body"
+      contenteditable="true"
+      v-on:input="sendPatch"
+    >
     </div>
   </div>
 </template>
 
 <script>
-const data = () => ({
-  conversation: {},
+import DiffMatchPatch from 'diff-match-patch';
+
+// Declare constants
+const initialProps = Object.freeze(['content', 'version']);
+const updateProps = Object.freeze(['type', 'version', 'patch', 'cursor_delta']);
+const updateTypes = Object.freeze({
+  Edit: 0,
 });
+
+const dmp = new DiffMatchPatch();
+
+const data = () => ({
+  ws: null,
+  conversation: {},
+  checkpoint: '',
+  version: 0,
+  content: '',
+  patchBuffer: [],
+});
+
+const checkMsgProps = (msg, requiredProps) => {
+  const missingProps = [];
+  requiredProps.forEach((prop) => {
+    if (!(prop in msg)) {
+      missingProps.push(prop);
+    }
+  });
+
+  return missingProps.join();
+};
 
 function conversations() {
   return this.$store.state.conversations;
@@ -27,14 +59,127 @@ function activeConversation() {
   return parseInt(this.$route.params.id, 10);
 }
 
+function sendPatch() {
+  const patches = dmp.patch_make(this.content, this.conversationDOM.innerHTML);
+  patches.forEach((patch) => {
+    this.version += 1;
+    const patchStr = dmp.patch_toText([patch]);
+    if (patchStr === '') {
+      return;
+    }
+
+    const msg = {
+      type: updateTypes.Edit,
+      version: this.version,
+      patch: patchStr,
+      cursor_delta: 0,
+    };
+
+    this.patchBuffer.push(patchStr);
+    this.ws.send(JSON.stringify(msg));
+  });
+
+  this.content = this.conversationDOM.innerHTML;
+}
+
+function parseWSMessage(e) {
+  let msg = {};
+  try {
+    msg = JSON.parse(e.data);
+  } catch (err) {
+    console.log(`Unable to parse JSON data: ${err.message}`);
+    return;
+  }
+
+  if ('content' in msg) { // Initial message
+    const missingProps = checkMsgProps(msg, initialProps);
+    if (missingProps !== '') {
+      console.log(`Initial message missing required fields: ${missingProps}`);
+    }
+    this.checkpoint = msg.content;
+    this.version = msg.version;
+    this.content = this.checkpoint;
+    this.conversationDOM.innerHTML = this.content;
+  } else if ('type' in msg) {
+    const missingProps = checkMsgProps(msg, updateProps);
+    if (missingProps !== '') {
+      console.log(`Update message missing required fields: ${missingProps}`);
+    }
+
+    switch (msg.type) {
+      case updateTypes.Edit: {
+        const msgPatch = dmp.patch_fromText(msg.patch);
+        let content = '';
+        let versionDelta = 1;
+
+        if (msg.version <= this.version) {
+          [content] = dmp.patch_apply(msgPatch, this.checkpoint);
+          let i = 0;
+
+          while (i < this.patchBuffer.length) {
+            const patch = dmp.patch_fromText(this.patchBuffer[i]);
+            const [updatedContent, results] = dmp.patch_apply(patch, content);
+
+            if (!results[0]) {
+              this.patchBuffer.splice(i, 1);
+              versionDelta -= 1;
+            } else {
+              content = updatedContent;
+              i += 1;
+            }
+          }
+        } else if (msg.version === this.version + 1) {
+          [content] = dmp.patch_apply(msgPatch, this.conversationDOM.innerHTML);
+          this.checkpoint = content;
+          this.patchBuffer = [];
+        }
+
+        this.version += versionDelta;
+        this.content = content;
+        this.conversationDOM.innerHTML = this.content;
+        break;
+      }
+      default:
+        console.log(`Invalid update type: ${msg.type}`);
+    }
+  }
+}
+
 function created() {
   this.conversation = this.conversations[this.activeConversation];
+  try {
+    this.ws = new WebSocket(`ws://${process.env.VUE_APP_HOST}/patches/v1/connect/${this.activeConversation}`);
+  } catch (err) {
+    console.log(`Unable to create WebSocket connection: ${err.message}`);
+    return;
+  }
+
+  this.ws.onopen = () => {
+    console.log('OPEN WS');
+    this.ws.send('blah');
+  };
+  this.ws.onclose = () => {
+    console.log('CLOSE WS');
+    this.ws = null;
+  };
+  this.ws.onmessage = (e) => {
+    this.parseWSMessage(e);
+  };
+  this.ws.onerror = (e) => {
+    console.log(`ERROR: ${e.data}`);
+  };
+}
+
+function mounted() {
+  this.conversationDOM = this.$el.querySelector('#conversation-body');
+  this.conversationDOM.innerHTML = this.content;
 }
 
 export default {
   name: 'Conversation',
   data,
   created,
+  mounted,
   computed: {
     conversations,
     activeConversation,
@@ -46,6 +191,10 @@ export default {
     activeConversation() {
       this.conversation = this.conversations[this.activeConversation];
     },
+  },
+  methods: {
+    sendPatch,
+    parseWSMessage,
   },
 };
 </script>
