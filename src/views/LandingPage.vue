76bbe5c6c743a86a02c40d7e78c54daa73d830cc
Igor Veselinovic <igor@veselinovic.net>
2020-03-24 19:05:52 -0400

    Add basic "working" login code
    No logout or register yet.

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="landing-page">
+    <Login/>
+  </div>
+</template>
+
+<script>
+import Login from '../components/auth/Login.vue';
+
+export default {
+  name: 'Conversations',
+  components: {
+    Login,
+  },
+};
+</script>
