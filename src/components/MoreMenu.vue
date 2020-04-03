15799d499aac18b604b0a7ea184c86bf5a2d7825
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2020-04-02 23:15:01 -0400

    Create `MoreMenu` component

---
+++ src/components/MoreMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="popup-menu">
+    <div v-on:click="handleLogout">Logout</div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'MoreMenu',
+  methods: {
+    ...mapActions('user', ['logout']),
+    handleLogout() {
+      this.logout();
+    },
+  },
+};
+</script>
