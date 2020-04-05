174fe779c672b5c9975d1009fa2fbdd7621292a3
Igor Veselinovic <igor@veselinovic.net>
2020-04-05 13:55:17 -0400

    Add UI popup for creating conversations
    Pressing the "+" sign creates a pop-up with a form that has a "Name" and
    "Description" fields. Clicking "Create" sends a POST to ether and then
    refreshes the conversation list by dispatching the getAll action.

--- src/components/ConversationList.vue
+++ src/components/ConversationList.vue
@@ -5,12 +5,13 @@
         <h1 v-show="!user.avatar_url">{{ user.name }}</h1>
         <img v-show="user.avatar_url" class="avatar" v-bind:src="user.avatar_url">
         <div class="header-options">
-          <Add />
+          <Add v-on:click="createConversation = !createConversation"/>
           <More v-on:click="more = !more"/>
         </div>
       </div>
     </div>
     <MoreMenu v-show="more" />
+    <CreateConversationMenu v-show="createConversation"/>
     <div class="conversation-list">
       <ConversationItem
         v-for="id in conversationsSorted"
@@ -29,10 +30,12 @@ import { mapState, mapActions, mapMutations } from 'vuex';
 import ConversationItem from './ConversationItem.vue';
 import MoreMenu from './MoreMenu.vue';
 import Add from '../assets/add-24px.svg';
+import CreateConversationMenu from './CreateConversationMenu.vue';
 import More from '../assets/more_vert-24px.svg';
 
 const data = () => ({
   more: false,
+  createConversation: false,
 });
 
 function created() {
@@ -48,6 +51,7 @@ export default {
     Add,
     More,
     MoreMenu,
+    CreateConversationMenu,
   },
   data,
   created,
