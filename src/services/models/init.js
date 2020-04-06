a06e80310bf08b818e5d018ff018f812c3944290
Igor Veselinovic <igor@veselinovic.net>
2020-04-06 15:24:01 -0400

    Initialize content editable div to have child div
    This makes it so that all text nodes are in divs which simplifies some
    of the code.

--- src/services/models/init.js
+++ src/services/models/init.js
@@ -42,6 +42,9 @@ class Init extends WSMessage {
     state.checkpoint.latest = state.version;
     state.caret = new Caret(0, 0);
     state.checkpoint.version[state.version] = new Checkpoint(state.caret, {});
+    if (state.content === '') {
+      state.content = '<div></div';
+    }
     state.el.setInnerHTML(state.content);
     state.textSize = state.el.getTextSize();
 
